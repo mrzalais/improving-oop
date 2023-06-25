028e22013891eed0c7576de738263b984b7906ff
mrzalais <lejnieksedijs@gmail.com>
2023-06-25 22:07:42 +0300

    Add simple dependency injection container

--- public/index.php
+++ public/index.php
@@ -1,7 +1,29 @@
 <?php
 
-use App\PaymentGateway\Paddle\Transaction;
+declare(strict_types = 1);
 
-require __DIR__ . '/../vendor/autoload.php';
+use App\App;
+use App\Config;
+use App\Container;
+use App\Controllers\HomeController;
+use App\Router;
 
-phpinfo();
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+define('STORAGE_PATH', __DIR__ . '/../storage');
+define('VIEW_PATH', __DIR__ . '/../views');
+
+$container = new Container();
+$router    = new Router($container);
+
+$router
+    ->get('/', [HomeController::class, 'index']);
+
+(new App(
+    $router,
+    ['uri' => $_SERVER['REQUEST_URI'], 'method' => $_SERVER['REQUEST_METHOD']],
+    new Config($_ENV)
+))->run();
