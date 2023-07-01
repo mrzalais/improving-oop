8ffba555fcd16e1c2ed01a9c1d74e55a40f5a5b0
mrzalais <lejnieksedijs@gmail.com>
2023-07-01 09:50:12 +0300

    Add event listener for invoice createdAt field

--- app/Entity/Invoice.php
+++ app/Entity/Invoice.php
@@ -5,18 +5,23 @@ declare(strict_types = 1);
 namespace App\Entity;
 
 use App\Enums\InvoiceStatus;
+use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
 use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\OneToMany;
+use Doctrine\ORM\Mapping\PrePersist;
 use Doctrine\ORM\Mapping\Table;
 
 #[Entity]
 #[Table('invoices')]
+#[HasLifecycleCallbacks]
 class Invoice
 {
     #[Id]
@@ -33,7 +38,7 @@ class Invoice
     private InvoiceStatus $status;
 
     #[Column(name: 'created_at')]
-    private \DateTime $createdAt;
+    private DateTime $createdAt;
 
     #[OneToMany(targetEntity: InvoiceItem::class, mappedBy: 'invoice', cascade: ['persist', 'remove'])]
     private Collection $items;
@@ -41,6 +46,12 @@ class Invoice
     public function __construct()
     {
         $this->items = new ArrayCollection();
+    }
+
+    #[PrePersist]
+    public function onPrePersist(LifecycleEventArgs $args): void
+    {
+        $this->createdAt = new DateTime();
     }
 
     public function getId(): int
@@ -84,16 +95,9 @@ class Invoice
         return $this;
     }
 
-    public function getCreatedAt(): \DateTime
+    public function getCreatedAt(): DateTime
     {
         return $this->createdAt;
-    }
-
-    public function setCreatedAt(\DateTime $createdAt): Invoice
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
     }
 
     /**
