7f931c21554fff418e58d748739da21708720de3
mrzalais <lejnieksedijs@gmail.com>
2023-07-04 11:54:06 +0300

    Add Slim PHPs PSR-7 and twig helper

--- app/Controllers/InvoiceController.php
+++ app/Controllers/InvoiceController.php
@@ -4,50 +4,23 @@ declare(strict_types = 1);
 
 namespace App\Controllers;
 
-use App\Attributes\Get;
-use App\Enums\InvoiceStatus;
-use App\Models\Invoice;
-use Carbon\Carbon;
-use Twig\Environment AS Twig;
+use App\Services\InvoiceService;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
 
 class InvoiceController
 {
-    public function __construct(private Twig $twig)
+    public function __construct(private InvoiceService $invoiceService)
     {
     }
 
-    #[Get('/invoices')]
-    public function index(): string
+    public function index(Request $request, Response $response, $args): Response
     {
-        $invoices = Invoice::query()
-            ->where('status', InvoiceStatus::Paid)
-            ->get()
-            ->map(
-                fn(Invoice $invoice) => [
-                    'invoiceNumber' => $invoice->invoice_number,
-                    'amount'        => $invoice->amount,
-                    'status'        => $invoice->status->toString(),
-                    'dueDate'       => $invoice->due_date->toDateTimeString(),
-                ]
-            )
-            ->toArray();
-
-        return $this->twig->render('invoices/index.twig', ['invoices' => $invoices]);
-    }
-
-
-    #[Get('/invoices/new')]
-    public function create(): void
-    {
-        $invoice = new Invoice();
-
-        $invoice->invoice_number = 5;
-        $invoice->amount         = 20;
-        $invoice->status         = InvoiceStatus::Pending;
-        $invoice->due_date       = (new Carbon())->addDay();
-
-        $invoice->save();
-
-        echo $invoice->id . ', ' . $invoice->due_date->format('m/d/Y');
+        return Twig::fromRequest($request)->render(
+            $response,
+            'invoices/index.twig',
+            ['invoices' => $this->invoiceService->getPaidInvoices()]
+        );
     }
 }
