91ee4705ab320302241f20dcfb4dc3424b5c9167
mrzalais <lejnieksedijs@gmail.com>
2023-07-05 11:33:24 +0300

    Extract container to config file

--- public/index.php
+++ public/index.php
@@ -2,19 +2,10 @@
 
 declare(strict_types = 1);
 
-use App\Config;
-use App\Controllers\HomeController;
-use App\Controllers\InvoiceController;
-use DI\Container;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\ORMSetup;
 use Dotenv\Dotenv;
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
-use Twig\Extra\Intl\IntlExtension;
-
-use function DI\create;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../configs/path_constants.php';
@@ -22,7 +13,8 @@ require __DIR__ . '/../configs/path_constants.php';
 $dotenv = Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
-$container = new Container();
+$container = require CONFIG_PATH . '/container.php';
+$router = require CONFIG_PATH . '/routes.php';
 
 AppFactory::setContainer($container);
 
