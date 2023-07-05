c895cae9c615e17e2e8fab4d1c3a3f08882e73fb
mrzalais <lejnieksedijs@gmail.com>
2023-07-05 11:33:59 +0300

    Add .env key for environment

--- app/Config.php
+++ app/Config.php
@@ -16,22 +16,13 @@ class Config
     {
         $this->config = [
             'db' => [
-                'host'      => $env['DB_HOST'],
-                'username'  => $env['DB_USER'],
-                'password'  => $env['DB_PASS'],
-                'database'  => $env['DB_DATABASE'],
-                'driver'    => $env['DB_DRIVER'] ?? 'mysql',
-                'charset'   => 'utf8',
-                'collation' => 'utf8_unicode_ci',
-                'prefix'    => '',
+                'host'     => $env['DB_HOST'],
+                'user'     => $env['DB_USER'],
+                'password' => $env['DB_PASS'],
+                'dbname'   => $env['DB_DATABASE'],
+                'driver'   => $env['DB_DRIVER'] ?? 'pdo_mysql',
             ],
-            'mailer' => [
-                'dsn' => $env['MAILER_DSN'] ?? '',
-            ],
-            'apiKeys' => [
-                'emailable'                     => $env['EMAILABLE_API_KEY'] ?? '',
-                'abstract_api_email_validation' => $env['ABSTRACT_API_EMAIL_VALIDATION_API_KEY'] ?? '',
-            ],
+            'environment' => $env['APP_ENVIRONMENT'] ?? 'production',
         ];
     }
 
