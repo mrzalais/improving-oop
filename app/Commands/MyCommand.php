ec2a17a9ed21856b775925968f62fdef12a5da77
mrzalais <lejnieksedijs@gmail.com>
2023-07-06 11:46:19 +0300

    Refactor commands to configs

--- app/Commands/MyCommand.php
+++ app/Commands/MyCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Commands;
 
+use App\Services\InvoiceService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -13,9 +14,14 @@ class MyCommand extends Command
     protected static $defaultName = 'app:my-command';
     protected static $defaultDescription = 'My command';
 
+    public function __construct(private readonly InvoiceService $invoiceService)
+    {
+        parent::__construct();
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->write('Hello world', true);
+        $output->write('Paid invoices: ' . count($this->invoiceService->getPaidInvoices()), true);
 
         return Command::SUCCESS;
     }
--- myapp.php
+++ myapp.php
@@ -6,19 +6,6 @@ use App\Commands\MyCommand;
 use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
 use Doctrine\Migrations\Configuration\Migration\PhpFile;
 use Doctrine\Migrations\DependencyFactory;
-use Doctrine\Migrations\Tools\Console\Command\CurrentCommand;
-use Doctrine\Migrations\Tools\Console\Command\DiffCommand;
-use Doctrine\Migrations\Tools\Console\Command\DumpSchemaCommand;
-use Doctrine\Migrations\Tools\Console\Command\ExecuteCommand;
-use Doctrine\Migrations\Tools\Console\Command\GenerateCommand;
-use Doctrine\Migrations\Tools\Console\Command\LatestCommand;
-use Doctrine\Migrations\Tools\Console\Command\ListCommand;
-use Doctrine\Migrations\Tools\Console\Command\MigrateCommand;
-use Doctrine\Migrations\Tools\Console\Command\RollupCommand;
-use Doctrine\Migrations\Tools\Console\Command\StatusCommand;
-use Doctrine\Migrations\Tools\Console\Command\SyncMetadataCommand;
-use Doctrine\Migrations\Tools\Console\Command\UpToDateCommand;
-use Doctrine\Migrations\Tools\Console\Command\VersionCommand;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
@@ -33,27 +20,18 @@ $config = new PhpFile(CONFIG_PATH . '/migrations.php');
 
 $dependencyFactory = DependencyFactory::fromEntityManager($config, new ExistingEntityManager($entityManager));
 
+$migrationCommands = require CONFIG_PATH . '/migration_commands.php';
+$customCommands = require CONFIG_PATH . '/commands.php';
+
 $commands = [
-    new CurrentCommand($dependencyFactory),
-    new DumpSchemaCommand($dependencyFactory),
-    new ExecuteCommand($dependencyFactory),
-    new GenerateCommand($dependencyFactory),
-    new LatestCommand($dependencyFactory),
-    new MigrateCommand($dependencyFactory),
-    new RollupCommand($dependencyFactory),
-    new StatusCommand($dependencyFactory),
-    new VersionCommand($dependencyFactory),
-    new UpToDateCommand($dependencyFactory),
-    new SyncMetadataCommand($dependencyFactory),
-    new ListCommand($dependencyFactory),
-    new DiffCommand($dependencyFactory),
-    new MyCommand(),
+    $container->get(MyCommand::class),
 ];
 
 $application = new Application('App', '1.0');
 
 ConsoleRunner::addCommands($application, new SingleManagerProvider($entityManager));
 
-$application->addCommands($commands);
+$application->addCommands($migrationCommands($dependencyFactory));
+$application->addCommands(array_map(fn($command) => $container->get($command), $customCommands));
 
 $application->run();
